3b22895e34681ef11980eda44f14214a3355c4ca
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-06-01 20:58:13 -0500

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+# app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+# Initialize Flask application
+app = Flask(__name__)
+app.config.from_object(Config)  # Load configuration from Config class
+
+# Initialize SQLAlchemy for database management
+db = SQLAlchemy(app)
+
+# Initialize Flask-Login for user session management
+login = LoginManager(app)
+login.login_view = 'login'  # Set the login view to 'login'
+
+# Import routes and models
+from app import routes, models
