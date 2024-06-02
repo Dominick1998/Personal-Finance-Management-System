55bc06a867f74a389c7991171130a1fd3fba1bd7
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-06-01 20:55:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+# config.py
 
+import os
+
+class Config:
+    """
+    Configuration class for Flask application.
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable modification tracking for SQLAlchemy
