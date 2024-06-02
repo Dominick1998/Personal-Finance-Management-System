27428c4c45d0cd267d1c907ce8023c6dd130a5de
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-06-01 20:55:05 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
+# run.py
 
+# Import the application instance from the app package
+from app import app
+
+# Main entry point for the Flask application
+if __name__ == "__main__":
+    app.run(debug=True)  # Run the application in debug mode
