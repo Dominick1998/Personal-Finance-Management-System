f352466c076916671709958478151fd35fbdd4ca
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-06-03 23:28:54 -0500

    Update __init__.py
    Include Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import Config
 
 # Initialize Flask application
@@ -11,6 +12,9 @@ app.config.from_object(Config)  # Load configuration from Config class
 
 # Initialize SQLAlchemy for database management
 db = SQLAlchemy(app)
+
+# Initialize Flask-Migrate for database migrations
+migrate = Migrate(app, db)
 
 # Initialize Flask-Login for user session management
 login = LoginManager(app)
