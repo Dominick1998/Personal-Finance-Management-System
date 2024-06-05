f298c43aa93aa3360e3f4332e7bd25cb07770003
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-06-04 22:23:11 -0500

    Create script.js

---
+++ app/static/js/script.js
@@ -0,0 +1,7 @@
+// app/static/js/script.js
+
+// Add your custom JavaScript here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is loaded and ready.");
+});
