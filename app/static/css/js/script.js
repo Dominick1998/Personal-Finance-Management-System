1ad3831e8aa8dfabaddefde2628d6a13aae41c26
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-06-04 22:23:22 -0500

    Delete app/static/css/js directory

--- app/static/css/js/script.js
+++
@@ -1,7 +0,0 @@
-// app/static/js/script.js
-
-// Add your custom JavaScript here
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript is loaded and ready.");
-});
