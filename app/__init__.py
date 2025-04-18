a1fa6ef83d3a2a0cb6f0df824de050723b6ecf6b
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2025-04-18 15:55:09 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,10 @@
 # - Configuring extensions
 # - Importing and defining package-level variables or functions
 
-# The presence of this file allows the import from the 'app' package 
-# in other parts of your project, e.g., 'from app import some_module'.
+'''
+The presence of this file allows the import from the 'app' package 
+in other parts of your project, e.g., 'from app import some_module'.
+'''
 
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
